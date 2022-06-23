7e35af82a25fe735c4543b0c9f0993661beee9b3
Hanif Dwy Putra S <47862061+hansputera@users.noreply.github.com>
2022-06-23 10:34:25 +0000

    feat: added scripts.genKey, and public key
    Signed-off-by: GitHub <noreply@github.com>

---
+++ scripts/genKey.mjs
@@ -0,0 +1,110 @@
+import { generateKeyPairSync } from 'node:crypto';
+import dotenv from 'dotenv';
+import * as readline from 'node:readline/promises';
+import {cwd, stdin, stdout} from 'node:process';
+import { resolve } from 'node:path';
+import { existsSync, mkdirSync, readFileSync, writeFileSync } from 'node:fs';
+import { constants, deflateSync } from 'node:zlib';
+
+dotenv.config();
+
+/**
+ * Ask a question.
+ * @param {readline.Interface} rl Readline interface.
+ * @param {string} question A question want to ask.
+ * @param {Function} validateFunc A validator to validate the answer.
+ * @return {Promise<string>}
+ */
+async function ask(rl, question, validateFunc = async () => true) {
+  let isValid = false;
+  let answer = '';
+
+  while (!isValid) {
+    answer = await rl.question(question);
+    const validate = await validateFunc(answer).catch((e) => ({
+      'error': e.message,
+    }));
+
+    isValid = !!!validate.error;
+
+    if (!isValid) {
+      answer = '';
+      console.log(validate.error || 'Unexpected answer, try again!\n\n');
+    }
+  }
+
+  return answer;
+}
+
+/**
+ * Generate key.
+ * @return {Promise<void>}
+ */
+async function generateKey() {
+  if (!existsSync(resolve(cwd(), 'keys'))) mkdirSync(resolve(cwd(), 'keys'));
+
+  console.log('Welcome to Key Generator Script');
+  console.log('Date time:', new Date());
+
+  const collectedData = {
+    'key': Buffer.alloc(0),
+  };
+
+  const readlineInterface = readline.createInterface({
+    input: stdin,
+    output: stdout,
+  });
+
+  const secretKey = await ask(
+      readlineInterface,
+      'Give me your secret key: ',
+      async (value) => {
+        if (value.length < 5) {
+          throw new Error('Secret key must be higher than 5 chars!');
+        } else {
+          return Promise.resolve({});
+        }
+      },
+  );
+
+  console.log('\nGenerating key..');
+  const keyPair = generateKeyPairSync('rsa', {
+    modulusLength: 4096,
+    privateKeyEncoding: {
+        type: 'pkcs1',
+        format: 'pem',
+        passphrase: secretKey,
+        cipher: 'aes-256-cbc',
+    },
+    publicKeyEncoding: {
+        type: 'pkcs1',
+        format: 'pem',
+    }
+  });
+
+  console.log('Writing public key, and the private key to \'keys\' folder');
+  writeFileSync(resolve(cwd(), 'keys', 'private.key.pem'), keyPair.privateKey);
+  writeFileSync(resolve(cwd(), 'keys', 'public.key.pem'), keyPair.publicKey);
+
+  let compressedPrivateKey = deflateSync(keyPair.privateKey, {
+    level: constants.Z_BEST_COMPRESSION,
+    memLevel: constants.Z_RLE,
+  });
+  
+  compressedPrivateKey = deflateSync(compressedPrivateKey, {
+    level: constants.Z_BEST_COMPRESSION,
+    memLevel: constants.Z_RLE,
+  });
+
+  const contentsOnEnv = readFileSync(resolve(cwd(), '.env'), { encoding: 'utf8' });
+  const contentsParsed = dotenv.parse(contentsOnEnv.concat(
+    '\n', 'PRIVATE_KEY=', compressedPrivateKey.toString('hex'),
+  ));
+
+  writeFileSync(resolve(cwd(), '.env'), Object.keys(contentsParsed).map((x) => `${x}=${contentsParsed[x]}`).join('\n'));
+
+  console.log('Done! See you next time!');
+  readlineInterface.close();
+}
+
+generateKey();
