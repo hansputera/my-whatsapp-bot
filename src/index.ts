ed48ce6ac7ec54aa4bd1b42b14452884229ec37e
Hanif Dwy Putra S <47862061+hansputera@users.noreply.github.com>
2022-06-29 09:15:04 +0000

    feat: use custom authState file manager
    Signed-off-by: GitHub <noreply@github.com>

--- src/index.ts
+++ src/index.ts
@@ -6,58 +6,71 @@ import type { Boom } from '@hapi/boom';
 
 import * as qr from 'qrcode';
 import {
-	useSingleFileAuthState,
 	DisconnectReason,
-	BaileysEventMap,
-	AuthenticationCreds,
+	AuthenticationState,
 } from '@adiwajshing/baileys';
 import { Client } from './objects';
+import { useAuthStateFile } from './authState';
 
 /**
  * Init baileys connection
  *
- * @return {void}
+ * @return {Promise<void>}
  */
-function initSock(): void {
-	const { state, saveState } = useSingleFileAuthState(
-		resolvePath(__dirname, '..', 'auth.json'),
-	);
+async function initSock(): Promise<void> {
+	try {
+		const { state, saveState } = await useAuthStateFile(
+			resolvePath(__dirname, '..', 'auth.json.enc'),
+		);
 
-	const client = new Client({
-		'auth': state,
-	});
+		const client = new Client({
+			'auth': state as AuthenticationState,
+		});
 
-	client.baileys.ev.on('connection.update', (conn) => {
-		if (conn.qr) {
-			client.logger.info('QR Generated');
-			qr.toFile(resolvePath(__dirname, '..', 'qr.png'), conn.qr);
-		} else if (conn.connection && conn.connection === 'close') {
-			if (
-				(conn.lastDisconnect?.error as Boom).output.statusCode !==
-				DisconnectReason.loggedOut
-			) {
-				client.logger.info('Trying to reconnect');
-				client.modules.free();
-				for (const listener of client.modules.listens) {
-					client.baileys.ev.removeAllListeners(
-						listener as keyof BaileysEventMap<AuthenticationCreds>,
-					);
+		client.baileys.ev.on('connection.update', (conn) => {
+			if (conn.qr) {
+				client.logger.info('QR Generated');
+				qr.toFile(resolvePath(__dirname, '..', 'qr.png'), conn.qr);
+			} else if (conn.connection && conn.connection === 'close') {
+				if (
+					(conn.lastDisconnect?.error as Boom).output.statusCode !==
+					DisconnectReason.loggedOut
+				) {
+					client.logger.info('Trying to reconnect');
+					client.modules.free();
+					for (const listener of client.modules.listens) {
+						client.baileys.ev.removeAllListeners(
+							listener,
+						);
+					}
+					initSock();
 				}
-				initSock();
+				if (existsSync(resolvePath(__dirname, '..', 'qr.png'))) {
+					unlinkSync(resolvePath(__dirname, '..', 'qr.png'));
+				}
+			} else if (conn.connection && conn.connection === 'open') {
+				client.logger.info('WebSocket opened');
 			}
-			if (existsSync(resolvePath(__dirname, '..', 'qr.png'))) {
-				unlinkSync(resolvePath(__dirname, '..', 'qr.png'));
-			}
-		} else if (conn.connection && conn.connection === 'open') {
-			client.logger.info('WebSocket opened');
+		});
+
+		client.baileys.ev.on('creds.update', () => {
+			saveState().catch((err) => {
+				client.logger.warn(
+					'Fail to save credentials, reason:',
+					err.message,
+				);
+			});
+		});
+
+		// start the module
+		client.modules.loads();
+		client.modules.loadEvents();
+	} catch (err) {
+		if (err instanceof Error) {
+			console.error(err.message);
+			process.exit(1);
 		}
-	});
-
-	client.baileys.ev.on('creds.update', saveState);
-
-	// start the module
-	client.modules.loads();
-	client.modules.loadEvents();
+	}
 }
 
 process.setMaxListeners(20);
